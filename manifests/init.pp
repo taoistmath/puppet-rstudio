8cfe5c3908f51c1299808b76de9a41078fdfca2c
taoistmath <greg.fogelberg@gmail.com>
2014-12-13 11:00:49 -0800

    boxen module for RStudio

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,12 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Install RStudio.app into /Applications.
+#
+# Examples
+#
+# include rstudio
+class rstudio {
+  package { 'RStudio':
+    provider => 'compressed_app',
+    source   => 'http://download1.rstudio.org/RStudio-0.98.1091.dmg',
+    flavor   => 'dmg',
+  }
 }
