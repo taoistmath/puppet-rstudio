30fc5c83fb61b7c505bf0f93ef0f8c4c1fcfee2d
taoistmath <greg.fogelberg@gmail.com>
2014-12-13 11:35:35 -0800

    updating provider in init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,10 +3,9 @@
 # Examples
 #
 # include rstudio
-class rstudio {
+class rstudio($version = '0.98.1091') {
   package { 'RStudio':
-    provider => 'compressed_app',
-    source   => 'http://download1.rstudio.org/RStudio-0.98.1091.dmg',
-    flavor   => 'dmg',
+    provider => 'appdmg',
+    source   => "http://download1.rstudio.org/RStudio-${version}.dmg",
   }
 }
--- spec/classes/rstudio_spec.rb
+++ spec/classes/rstudio_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
-  describe 'rstudio' do
-    it do
-      should contain_class('rstudio')
-      should contain_package('RStudio').with({
-        :provider => 'compressed_app',
-        :source => 'http://download1.rstudio.org/RStudio-0.98.1091.dmg',
-        :flavor => 'dmg',
-      })
-    end
+
+describe 'rstudio' do
+	 version = '0.98.1091'
+	 it 'contains the RStudio package' do
+	 	should contain_package('RStudio').with({
+	 		:provider => 'appdmg',
+	 		:source => "http://download1.rstudio.org/RStudio-#{version}.dmg"
+	 	})
+	 end
 end
